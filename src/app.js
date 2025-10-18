6574b6f9ef6c100fc58b4b1e26d9e83626445d40
Humza920 <humza.rehman.779@gmail.com>
2025-10-18 14:49:27 +0500

    TRAINING (Start FrontEnd)

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const paymentRouter = require("./routes/paymentRoutes")
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5174",
+    origin: "http://localhost:5173",
     credentials:true
 }))
 app.use(cookieParser())
